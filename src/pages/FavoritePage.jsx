b8c7c57c878b8309dcef6c8bda5b125d3b2d4a0e
Thirana <t.embuldeniya@gmail.com>
2024-06-09 06:29:16 +0530

    updated inital part of HomePage and FavoritePage

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,5 +1,19 @@
 const FavoritePage = () => {
-  return <div>FavoritePage</div>;
+  const fav = false;
+
+  return (
+    <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen">
+      <div className="max-w-screen-lg mx-auto">
+        <p className="font-bold text-3xl md:text-5xl my-4">My Favorites</p>
+
+        {!fav && (
+          <div className="h-[80vh] flex flex-col items-center gap-4">
+            <img src="/404.svg" className="h-3/4" alt="404 svg" />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FavoritePage;
